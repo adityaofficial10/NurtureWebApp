e3e9124521a6d12c3ba6ceacd79866a045f5f700
ADITYA R RUDRA <adityaberasia@gmail.com>
2020-11-30 16:06:51 +0530

    Mentor and Student models added...

--- server/datastore.js
+++ server/datastore.js
@@ -13,3 +13,78 @@ console.log("Database connected successfully..");
 //});
 
 autoIncrement.initialize(connection);
+
+
+const mentorSchema = new Schema({
+
+  name:{
+    type:String,
+    required:true
+  },
+  age:{
+    type:Number,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  available:{
+    type:Boolean,
+    default:true
+  },
+  contactNumber:{
+    type:String,
+  },
+  dateOfBirth:{
+    type:Date,
+    required:true
+  },
+  mentorPassword:{
+    type:String,
+    required:true
+  }
+});
+
+mentorSchema.plugin(autoIncrement.plugin, {
+    model: 'Mentor',
+    startAt: 1,
+    incrementBy: 1
+});
+
+const studentSchema = new Schema({
+
+  name:{
+    type:String,
+    required:true
+  },
+  age:{
+    type:Number,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+
+  contactNumber:{
+    type:String,
+  },
+  dateOfBirth:{
+    type:Date,
+    required:true
+  },
+  studentPassword:{
+    type:String,
+    required:true
+  }
+});
+
+studentSchema.plugin(autoIncrement.plugin, {
+    model: 'Student',
+    startAt: 1,
+    incrementBy: 1
+});
+
+const Mentor = connection.model('Mentor',mentorSchema);
+const Student = connection.model('Student',studentSchema);
