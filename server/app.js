0c5ee815305775866bb46a02b6deb880debaf1f9
ADITYA R RUDRA <adityaberasia@gmail.com>
2020-12-01 17:22:52 +0530

    Server configuration added...

--- server/app.js
+++ server/app.js
@@ -1,2 +1,29 @@
-//This file consists of the basic configuration and routes of the backend server...
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const multer = require("multer");
+const fileupload = require("express-fileupload");
+const path = require("path");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const fs = require('fs');
+const _ = require('lodash');
+const aws = require("aws-sdk");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(morgan('dev'));
+
+
+const data = require("./datastore.js");
+
+app.listen(3000,function(err){
+  if(err)
+  console.error(err);
+  else
+  console.log("Server running on port 3000...");
+});
