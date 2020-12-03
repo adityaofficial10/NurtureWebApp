f7459acb2148729fb698b586dbd3fced0485c9a5
ADITYA R RUDRA <adityaberasia@gmail.com>
2020-12-03 11:58:03 +0530

    Routes added..

--- server/app.js
+++ server/app.js
@@ -19,7 +19,44 @@ app.use(bodyParser.urlencoded({
 app.use(morgan('dev'));
 
 
-const data = require("./datastore.js");
+const { Mentor, User, Event, Request, Slot } = require("./datastore.js");
+
+app.get("/mentors",function(req,res){
+
+  Mentor.find({},'name email available contactNumber').then((d)=>{
+    res.send(d);
+  }).catch((error)=>{
+    console.error(error);
+  });
+
+});
+
+app.post("/request",function(req,res){
+
+   var d1 = req.body.startTime;
+   var d2 = req.body.endTime;
+   const mtr = Mentor.find({name:req.body.mentorName}).then((data)=>{
+
+     const r = new Request({
+       title: req.body.title,
+       description: req.body.description,
+       mentor:data._fields._id,
+       student:,
+       date:req.body.date,
+       startTime:d1,
+       endTime:d2,
+       duration:d2-d1
+     });
+     r.save().then((succ)=>{
+       console.log("Success");
+     }).catch((error)=>{
+       console.error(error);
+     });
+
+   });
+
+});
+
 
 app.listen(3000,function(err){
   if(err)
