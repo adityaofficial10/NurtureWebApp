696af88910b9afe443b1d3106d270f7fa8f46015
Aditya R Rudra <adityaberasia@gmail.com>
2021-05-05 00:11:23 +0530

    Code for user and mentor profiles added to server.

---
+++ server/app/api/controllers/profiles.js
@@ -0,0 +1,41 @@
+'use strict';
+const mentorModel = require('../models/mentors');
+const userModel = require('../models/Users');
+
+module.exports = {
+
+  userProfile: async function(req, res, next) {
+    const userEmail = req.query.email;
+    if (!userEmail) {
+      res.json({status: 404, message: 'The user email ID was not provided.',
+        data: null});
+    } else {
+      userModel.findOne({email: userEmail}, function(_err, userInfo) {
+        if (_err)
+          throw _err;
+        if (!userInfo)
+          res.json({status: 404,
+            message: 'There is no user account linked with this email.',
+            data: null});
+        res.json({status: 200, message: 'User Found!', data: userInfo});
+      });
+    }
+  },
+  mentorProfile: async function(req, res, next) {
+    const mentorEmail = req.query.email;
+    if (!mentorEmail) {
+      res.json({status: 404, message: 'The mentor email ID was not provided.',
+        data: null});
+    } else {
+      mentorModel.findOne({email: mentorEmail}, function(_err, mentorInfo) {
+        if (_err)
+          throw _err;
+        if (!mentorInfo)
+          res.json({status: 404,
+            message: 'There is no mentor account linked with this email.',
+            data: null});
+        res.json({status: 200, message: 'Mentor Found!', data: mentorInfo});
+      });
+    }
+  },
+};
