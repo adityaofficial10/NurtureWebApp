1369014a3280570e4223afeaf8c57d15a6547401
Aditya R Rudra <adityaberasia@gmail.com>
2021-05-04 23:58:00 +0530

    Decision pages updated..

---
+++ client/src/Components/Authentication/Decision/signupDecision.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
+import './../../../assets/scss/style.scss';
+import Aux from "../../../hoc/_Aux";
+import Breadcrumb from "../../../App/layout/AdminLayout/Breadcrumb";
+
+class signupDecision extends React.Component {
+    render () {
+        return(
+            <Aux>
+                <Breadcrumb/>
+                <div className="auth-wrapper">
+                    <div className="auth-content">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <div className="mb-4">
+                                    <i className="feather icon-user-plus auth-icon"/>
+                                </div>
+                                <h3 className="mb-4">Sign up</h3>
+                                <Link to="/auth/signup-mentor">
+                                    <button className="btn btn-primary shadow-2 mb-4">Mentor</button>
+                                </Link>
+                                <Link to="/auth/signup-student">
+                                    <button className="btn btn-primary shadow-2 mb-4">Mentee</button>
+                                </Link>
+                                <p className="mb-0 text-muted">Allready have an account? <NavLink to="/auth/signin">Login</NavLink></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default signupDecision;
