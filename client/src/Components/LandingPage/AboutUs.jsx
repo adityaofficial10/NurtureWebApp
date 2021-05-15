8ff254c49430973b008baabca174410f00da8ca9
Niraj Nandish <nirajnandish@icloud.com>
2021-05-15 15:58:00 +0400

    Pro design About us (1/2)

--- client/src/Components/LandingPage/AboutUs.jsx
+++ client/src/Components/LandingPage/AboutUs.jsx
@@ -9,12 +9,12 @@ export default function AboutUs() {
         // [] - Make it more pro
         <div className='p-sm-5' id='about-us' name='about-us'>
             <div className='container'>
-                <div className='row'>
-                    <div className='col-md order-md-2 p-0 wow fadeInRight' id='about-us-img'>
+                <div className='row flex-md-nowrap align-items-center wow fadeInUp'>
+                    <div className='col-md order-md-2 p-0' id='about-us-img'>
                         <img src='https://static.wixstatic.com/media/f2a190_f1f12c856e7f4492871b0669f395a000~mv2.jpg/v1/fill/w_613,h_600,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01/f2a190_f1f12c856e7f4492871b0669f395a000~mv2.webp' />
                     </div>
-                    <div className='col-md order-md-1 py-3 px-5 mr-md-5 wow fadeInLeft' id='about-us-content'>
-                        <h1 className='mb-5'>ABOUT US</h1>
+                    <div className='col-md order-md-1 pl-4 py-5' id='about-us-content'>
+                        <h1 className='mb-3'>ABOUT US</h1>
                         <p>
                             Here at Nurture, we recognize the importance of individual involvement
                             to power their communities and themselves. We work to empower women and
